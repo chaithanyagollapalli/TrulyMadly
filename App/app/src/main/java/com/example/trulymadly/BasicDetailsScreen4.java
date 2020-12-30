2688f60746f556dbf0e1a12e0afa0f52713fc118
prajwal799 <prajwalgoual1999@gmail.com>
2020-12-30 13:03:26 +0530

    Screen5

--- App/app/src/main/java/com/example/trulymadly/BasicDetailsScreen4.java
+++ App/app/src/main/java/com/example/trulymadly/BasicDetailsScreen4.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class BasicDetailsScreen4 extends AppCompatActivity {
+import static com.example.trulymadly.R.layout.activity_basic_detail_screen_4;
 
+public class BasicDetailsScreen4 extends AppCompatActivity {
+    private Button mBtnNext2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic_detail_screen_4);
-
+        setContentView(activity_basic_detail_screen_4);
+        mBtnNext2 = findViewById(R.id.btn2Next);
+        mBtnNext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BasicDetailsScreen4.this, EducationInformation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
