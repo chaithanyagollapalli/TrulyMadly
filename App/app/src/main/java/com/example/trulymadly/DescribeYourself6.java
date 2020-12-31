dfa3e457b349e24714dfdd2e6afcbcc943b9784d
harsh <harshraga7@gmail.com>
2020-12-31 17:29:24 +0530

    main_screen_with_horizontal

--- App/app/src/main/java/com/example/trulymadly/DescribeYourself6.java
+++ App/app/src/main/java/com/example/trulymadly/DescribeYourself6.java
@@ -2,13 +2,27 @@ package com.example.trulymadly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DescribeYourself6 extends AppCompatActivity {
+    private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_describe_yourself6);
+
+        btnSave=findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(DescribeYourself6.this,Imageone.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
