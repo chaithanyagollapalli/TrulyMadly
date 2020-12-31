5c262d2a6f4158911c7edbc8daa5d3ce7ed19e66
harsh <harshraga7@gmail.com>
2020-12-31 08:11:13 +0530

    main_screen

--- App/app/src/main/java/com/example/trulymadly/BasicDetailsScreen4.java
+++ App/app/src/main/java/com/example/trulymadly/BasicDetailsScreen4.java
@@ -8,12 +8,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class BasicDetailsScreen4 extends AppCompatActivity {
+    private Button btn2Next;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_detail_screen_4);
+        btn2Next=findViewById(R.id.btn2Next);
+        btn2Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(BasicDetailsScreen4.this,Imageone.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
