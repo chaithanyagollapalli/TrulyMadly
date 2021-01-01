b51df6f0d54720d3227933f3a82b82e0fca5d354
harsh <harshraga7@gmail.com>
2021-01-01 20:17:47 +0530

    profiles_of_screen

--- App/app/src/main/java/com/example/trulymadly/AboutYouScreen3.java
+++ App/app/src/main/java/com/example/trulymadly/AboutYouScreen3.java
@@ -6,14 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class AboutYouScreen3 extends AppCompatActivity {
     private Button btnNext;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about__you_screen_3);
+        radioGroup=findViewById(R.id.radioGroup);
+
 
         btnNext = findViewById(R.id.btnNext);
         btnNext.setOnClickListener(new View.OnClickListener() {
