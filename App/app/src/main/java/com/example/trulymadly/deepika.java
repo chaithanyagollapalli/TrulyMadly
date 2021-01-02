a848a4391985784a4728fa9ef3e01406ba3b2d25
Chaithanya Gollapalli <chaitanyagcg.01@gmail.com>
2021-01-02 11:59:05 +0530

    Final App before presentation

--- App/app/src/main/java/com/example/trulymadly/deepika.java
+++ App/app/src/main/java/com/example/trulymadly/deepika.java
@@ -2,13 +2,54 @@ package com.example.trulymadly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class deepika extends AppCompatActivity {
+
+    private ImageView mImDoneDeepika;
+    private ImageView mImCloseDeepika;
+    private ImageButton mBtnMenuDeepika;
+    private ImageView mIvChatDeepika;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deepika);
+        mImDoneDeepika = findViewById(R.id.imDoneDeepika);
+        mImCloseDeepika = findViewById(R.id.imCloseDeepika);
+        mBtnMenuDeepika = findViewById(R.id.btnMenuDeepika);
+        mIvChatDeepika = findViewById(R.id.ivChatDeepika);
+        mImDoneDeepika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(deepika.this, sara.class);
+                startActivity(intent);
+            }
+        });
+        mImCloseDeepika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(deepika.this, sara.class);
+                startActivity(intent);
+            }
+        });
+        mBtnMenuDeepika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(deepika.this, NavigationMenuDrawer.class);
+                startActivity(intent);
+            }
+        });
+        mIvChatDeepika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(deepika.this, Conversation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- App/app/src/main/java/com/example/trulymadly/sara.java
+++ App/app/src/main/java/com/example/trulymadly/sara.java
@@ -2,13 +2,55 @@ package com.example.trulymadly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class sara extends AppCompatActivity {
+
+    private ImageView mImDoneSara;
+    private ImageView mImCloseSara;
+    private ImageButton mBtnMenuSara;
+    private ImageView mIvChatSara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sara);
+        mImDoneSara = findViewById(R.id.imDoneSara);
+        mImCloseSara = findViewById(R.id.imCloseSara);
+        mBtnMenuSara = findViewById(R.id.btnMenuSara);
+        mIvChatSara = findViewById(R.id.ivChatSara);
+
+        mImDoneSara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sara.this, katrina.class);
+                startActivity(intent);
+            }
+        });
+        mImCloseSara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sara.this, katrina.class);
+                startActivity(intent);
+            }
+        });
+        mBtnMenuSara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sara.this, NavigationMenuDrawer.class);
+                startActivity(intent);
+            }
+        });
+        mIvChatSara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(sara.this, Conversation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
